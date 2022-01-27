cb02fd35b819fd956573964b3c3cdcfb09613251
Ethan Isola <isolaethan@gmail.com>
2022-01-27 11:25:35 -0600

    commit tree 5180dddab72c7403da9aa2f2e982e3fb81d13887

--- index.js
+++ index.js
@@ -8,6 +8,69 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ function createEmployeeRecord(data) {
+    let employee = {
+        firstName: data[0],
+        familyName: data[1],
+        title: data[2],
+        payPerHour: data[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+}
+
+function createEmployeeRecords(data) {
+    let employees = []
+    for(let arr of data){
+        employees.push(createEmployeeRecord(arr))
+    }
+    return employees
+}
+
+function createTimeInEvent(stamp) {
+    let data = stamp.split(' ')
+    let timeIn = {
+    type: "TimeIn",
+    hour: Number(data[1]),
+    date: data[0]
+    }
+    this.timeInEvents.push(timeIn)
+    return this
+}
+
+function createTimeOutEvent(stamp) {
+    let data = stamp.split(' ')
+    let timeOut = {
+    type: "TimeOut",
+    hour: Number(data[1]),
+    date: data[0]
+    }
+    this.timeOutEvents.push(timeOut)
+    return this
+}
+
+function hoursWorkedOnDate(record, date){
+    let firstTime;
+    let secondTime;
+
+    for(let item of record.timeInEvents){
+        if(item.date == date){
+            firstTime = item.hour
+        }
+    }
+    for(let item of record.timeOutEvents){
+        if(item.date == date){
+            secondTime = item.hour
+        }
+    }
+    return this
+}
+
+function wagesEarnedOnDate(record, date){
+    console.log(hoursWorkedOnDate(record, date)*record.payPerHour)
+    return hoursWorkedOnDate(record, date)*record.payPerHour
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
